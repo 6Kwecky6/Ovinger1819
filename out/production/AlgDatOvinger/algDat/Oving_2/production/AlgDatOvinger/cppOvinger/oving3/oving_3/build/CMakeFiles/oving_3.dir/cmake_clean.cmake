4b8f40b3030fb401fc7d01c3847ad8253b30e11e
kevinah <kevinah@stud.ntnu.no>
2018-09-24 14:54:08 +0200

    Oving 5 algDat

---
+++ out/production/AlgDatOvinger/algDat/Oving_2/production/AlgDatOvinger/cppOvinger/oving3/oving_3/build/CMakeFiles/oving_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oving_3.dir/main.cpp.o"
+  "oving_3.pdb"
+  "oving_3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oving_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
